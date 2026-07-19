6bfbf6783e5bcc259611b7ff0bbd56653e65ee5b
PeterGab-PG2 <pgabriel@student.fullsail.edu>
2026-07-19 06:03:34 -0600

    Title for login screen complete, profile image next

--- Team1_SimpleMeal/app/src/main/java/com/example/simplemeal/MainActivity.kt
+++ Team1_SimpleMeal/app/src/main/java/com/example/simplemeal/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.simplemeal.ui.theme.BluePrimary
 import com.example.simplemeal.ui.theme.SimpleMealTheme
+import com.example.simplemeal.ui.theme.SnowDisplay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleMealTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    LoginScreen(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +44,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LoginScreen(name: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+
     )
+    {
+        Text(
+            text = "Simple Meal",
+            fontSize = 52.sp,
+            color = SnowDisplay,
+            modifier = modifier.background(BluePrimary)
+                .fillMaxWidth().padding(vertical = 40.dp),
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily.Serif,//till we import our own font
+            fontWeight = FontWeight.Bold
+
+        )
+        Image(painter = painterResource(id = R.drawable.profile_vector), contentDescription = "Profile Picture")
+        Text(
+            text = name,
+            fontSize = 24.sp,
+        )
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SimpleMealTheme {
-        Greeting("Android")
+        LoginScreen("Guest")
     }
 }
