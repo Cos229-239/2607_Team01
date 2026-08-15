f38bbad29b90a83bc4cb8a63b8c5da7b15d91c91
PeterGab-PG2 <pgabriel@student.fullsail.edu>
2026-08-14 21:24:32 -0600

    made keys for future screens

--- Team1_SimpleMeal/app/src/main/java/com/example/simplemeal/ui/NavKeys/NavScreenKeys.kt
+++ Team1_SimpleMeal/app/src/main/java/com/example/simplemeal/ui/NavKeys/NavScreenKeys.kt
@@ -6,4 +6,10 @@ data object route1_0_Login
 
 //data object route1_3_ContinueAsGuest
 
-//data object route2_0_ProfileScreen
+//data object route2_0_DashBoard
+
+//data object route3_0_SearchBrowse
+
+//data object route4_0_GroceryList
+
+//data object route5_0_Profile
