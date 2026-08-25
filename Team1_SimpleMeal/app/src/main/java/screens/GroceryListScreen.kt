a0c3f39b6bac7cc5766b8106c6e22a864d33b514
mairmadd <maddie.mair@icloud.com>
2026-08-24 19:51:26 -0600

    Make grocery list checkboxes interactive

--- Team1_SimpleMeal/app/src/main/java/screens/GroceryListScreen.kt
+++ Team1_SimpleMeal/app/src/main/java/screens/GroceryListScreen.kt
@@ -6,23 +6,40 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun GroceryListScreen()
 {
-    Column (modifier = Modifier.padding(16.dp))
+    var tomatoesChecked by remember { mutableStateOf(false) }
+    var chickenChecked by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.padding(16.dp))
     {
         Text(text = "Grocery List")
-       Row 
-       {
-        Checkbox(checked = false, onCheckedChange = null)
-        Text(text = "2 Tomatoes")
-        }
-        Row 
+
+        Row
         {
-            Checkbox(checked = false, onCheckedChange = null)
+            Checkbox(
+                checked = tomatoesChecked,
+                onCheckedChange = { tomatoesChecked = it }
+            )
+
+            Text(text = "2 Tomatoes")
+        }
+
+        Row
+        {
+            Checkbox(
+                checked = chickenChecked,
+                onCheckedChange = { chickenChecked = it }
+            )
+
             Text(text = "1 lb Chicken Breast")
         }
     }
